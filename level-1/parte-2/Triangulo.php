e57934b0b2b07aece794c2e4594ddc0dd437aa61
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-22 13:22:06 +0200

    deleted the constructor method in the subclasses

--- level-1/parte-2/Triangulo.php
+++ level-1/parte-2/Triangulo.php
@@ -1,11 +1,6 @@
 <?php
 class Triangulo extends Shape {
 
-//constructor heredando de la super clase//
-public function __construct(int|float $ancho, int|float $alto) {
-   parent::__construct ($ancho , $alto);
-}
-
 //metodo para calcular el area//
 public function calcularArea (): string {
     $area = ($this->getAncho() * $this->getAlto()) / 2;
